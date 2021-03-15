b69a95079a8b2e9eb945d3afff02484bf1d3317e
sdl60660 <learnersd@gmail.com>
2021-03-14 23:03:36 -0400

    Removed AV Club project and added hover effect

--- components/Projects.js
+++ components/Projects.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 import projectData from '../public/data/project_data.json';
 
 const Projects = () => {
+    const [featuredProject, setFeaturedProject] = useState(null);
+
+    useEffect(() => {
+
+    })
+
     return (
         <div className={styles.container} id={styles["project-container"]}>
             <div className={styles.row}>
@@ -13,15 +19,35 @@ const Projects = () => {
 
             <div className={styles.row}>
             {
-                projectData.map(({ project_link, title, image, added_classes }) => (
-                    <a key={title} target="_blank" href={project_link} className={styles.project}>
-                        <div className={styles["project__image-wrapper"]}>
-                        <div className={`${styles["project__preview-image"]} ${added_classes !== "" ? styles[added_classes] : ""}`}>
-                            <img src={`/img/${image}`} alt={title} />
-                        </div>
-                        <span className={styles.caption}>{title}</span>
-                        </div>
-                    </a>
+                projectData
+                    .filter(({ visible }) => visible === "true")
+                    .map(({ project_link, title, image, added_classes }) => (
+                        <a
+                            key={title}
+                            target="_blank"
+                            href={project_link}
+                            className={styles.project}
+                        >
+                            <div className={styles["project__image-wrapper"]}>
+                                <div
+                                    className={`${styles["project__preview-image"]} ${added_classes !== "" ? styles[added_classes] : ""}`}
+                                    data-info={JSON.stringify(title)}
+                                    onMouseEnter={(e) => {
+                                        setFeaturedProject(e.target.dataset.info);
+                                        // featuredProject.current = e.target.dataset.info;
+                                        // console.log(featuredProject.current)
+                                    }}
+                                    onMouseLeave={() => { setFeaturedProject(null) }}
+                                    style={{
+                                            opacity: featuredProject === null ? 0.8 : featuredProject === JSON.stringify(title) ? 1.0 : 0.5,
+                                            transform: featuredProject === JSON.stringify(title) ? "scale(1.05)" : "scale(1)"
+                                        }}
+                                >
+                                    <img src={`/img/${image}`} alt={title} data-info={JSON.stringify(title)} />
+                                </div>
+                                <span className={styles.caption}>{title.split('\n').map((chunk, i) => <div key={i}>{chunk}</div>)}</span>
+                            </div>
+                        </a>
                 ))
             }
             </div>

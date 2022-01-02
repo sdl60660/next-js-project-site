fe08827ff854de6bdf2d9e5052133e42bc458f65
sdl60660 <learnersd@gmail.com>
2022-01-01 20:14:50 -0500

    Updated favicon

--- components/Header.js
+++ components/Header.js
@@ -1,49 +1,59 @@
-import React from 'react';
-import Head from 'next/head'
+import React from "react";
+import Head from "next/head";
 
-const Header = ({ siteTitle, description="Sam Learner code/visualization portfolio site." }) => {
-    return (
-        <Head>
-            <meta charSet="utf-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-            <meta name="theme-color" content="#f3f3f3" />
+const Header = ({ siteTitle, description = "Sam Learner code/visualization portfolio site." }) => {
+  return (
+    <Head>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      <meta name="theme-color" content="#f3f3f3" />
 
-            <title>{siteTitle}</title>
-            <meta name="title" content={siteTitle} />
-            <meta name="description" content={description} />
+      <title>{siteTitle}</title>
+      <meta name="title" content={siteTitle} />
+      <meta name="description" content={description} />
 
-            <meta property="og:type" content="website" />
-            <meta property="og:title" content={siteTitle} />
-            <meta property="og:description" content={description} />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content={siteTitle} />
+      <meta property="og:description" content={description} />
 
-            <meta property="twitter:card" content="summary_large_image" />
-            <meta property="twitter:title" content={siteTitle} />
-            <meta property="twitter:description" content={description}></meta>
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:title" content={siteTitle} />
+      <meta property="twitter:description" content={description}></meta>
 
-            <link rel="preconnect" href="https://fonts.gstatic.com" />
-            
-            <link rel="preconnect" href="https://fonts.googleapis.com" />
-            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={"true"} />
-            <link href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
-            <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;500;600;700&display=swap" rel="stylesheet" />
-            <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+TC:wght@500;700&family=Roboto&display=swap" rel="stylesheet"></link>
-            <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+TC:wght@500;700&family=Yantramanav:wght@300;400;500;700&display=swap" rel="stylesheet"></link>
-            <script
-            async
-            src="https://www.googletagmanager.com/gtag/js?id=G-1CK69LSY3T"
-            />
-            <script
-            dangerouslySetInnerHTML={{
-                __html: `
+      <link rel="preconnect" href="https://fonts.gstatic.com" />
+
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={"true"} />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;500;600;700&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Noto+Serif+TC:wght@500;700&family=Roboto&display=swap"
+        rel="stylesheet"
+      ></link>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Noto+Serif+TC:wght@500;700&family=Yantramanav:wght@300;400;500;700&display=swap"
+        rel="stylesheet"
+      ></link>
+      <link rel="icon" href="/img/callout1.svg" />
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-1CK69LSY3T" />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
                     gtag('config', 'G-1CK69LSY3T');
                 `,
-            }}
-            />
-        </Head>
-    )
-}
+        }}
+      />
+    </Head>
+  );
+};
 
 export default Header;

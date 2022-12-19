51ed170a664fdb419eda5111dad796858792d842
Sam Learner <learnersd@gmail.com>
2022-12-19 11:07:03 -0500

    downloaded fonts to load in at asset/fonts/ rather than doing it with an inline request to google fonts on page load, fixed gtag setup according to next guidelines

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -26,32 +27,33 @@ export default function Document() {
           content="https://www.samlearner.com/img/site/callout1.svg"
         />
 
-        <link rel="preconnect" href="https://fonts.gstatic.com" />
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        {/* Downloaded fonts are now being loaded in in /asset/fonts */}
+        {/* <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
           href="https://fonts.gstatic.com"
           crossOrigin={"true"}
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Roboto+Mono:wght@400;500;600;700&family=Noto+Serif+TC:wght@500;700&family=Roboto&family=Noto+Serif+TC:wght@500;700&family=Yantramanav:wght@300;400;500;700&display=block"
+        /> */}
+        {/* <link
+          href="https://fonts.googleapis.com/css2?family=Red+Hat+Display:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Roboto+Mono:wght@400;500;600;700&family=Noto+Serif+TC:wght@500;700&family=Noto+Serif+TC:wght@500;700&family=Yantramanav:wght@300;400;500;700&display=block"
           rel="stylesheet"
           as="font"
-        />
-        <script
-          async
+        /> */}
+
+        {/* Global site tag (gtag.js) - Google Analytics */}
+        <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-1CK69LSY3T"
+          strategy="afterInteractive"
         />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `
-                    window.dataLayer = window.dataLayer || [];
-                    function gtag(){dataLayer.push(arguments);}
-                    gtag('js', new Date());
-                    gtag('config', 'G-1CK69LSY3T');
-                `,
-          }}
-        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-1CK69LSY3T');
+        `}
+        </Script>
       </Head>
       <body>
         <Main />
